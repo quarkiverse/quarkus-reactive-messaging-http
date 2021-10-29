c12d781226fb6cfd67eec12a480cc97d600c76a6
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-10-29 17:10:00 +0200

    add a native test

---
+++ integration-tests/src/test/java/io/quarkus/reactivemessaging/http/ReactiveMessagingHttpIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.reactivemessaging.http;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ReactiveMessagingHttpIT extends ReactiveMessagingHttpTest {
+}
